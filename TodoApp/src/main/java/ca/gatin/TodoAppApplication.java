95ce8ba5ef58b3011a2525f64059b157c3a12e26
Renat Gatin <renat.gatin@gmail.com>
2018-05-10 16:40:15 -0700

    typo

--- TodoApp/src/main/java/ca/gatin/TodoAppApplication.java
+++ TodoApp/src/main/java/ca/gatin/TodoAppApplication.java
@@ -3,7 +3,7 @@ package ca.gatin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication // uncomment it if you want to start it with embeded Server
+// @SpringBootApplication // uncomment it if you want to start it with embedded Server
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
