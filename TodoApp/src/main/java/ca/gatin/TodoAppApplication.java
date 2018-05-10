8a7af25d5757bbdd973e759db15429dc1abe140a
Owner <Owner@RGatin>
2018-05-10 16:24:09 -0700

    Prevent Spring boot startup with embedded servlet container

--- TodoApp/src/main/java/ca/gatin/TodoAppApplication.java
+++ TodoApp/src/main/java/ca/gatin/TodoAppApplication.java
@@ -3,7 +3,7 @@ package ca.gatin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication // uncomment it if you want to start it with embeded Server
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
