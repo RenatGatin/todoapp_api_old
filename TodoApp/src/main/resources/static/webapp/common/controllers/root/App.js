ae93286bf1bd25fa93fb7d99837572b700da3e39
Renat Gatin <renat.gatin@gmail.com>
2018-06-05 21:17:25 -0700

    Generated bower.json. Added angular-animate library.

--- TodoApp/src/main/resources/static/webapp/common/controllers/root/App.js
+++ TodoApp/src/main/resources/static/webapp/common/controllers/root/App.js
@@ -2,7 +2,7 @@
 	
 	angular.module('todoapp.routers', ['ui.router', 'uiRouterStyles']);
 	angular.module('todoapp.controllers', []);
-	angular.module('todoapp', ['todoapp.routers','todoapp.controllers', 'ngCookies', 'oc.lazyLoad', 'toaster'])
+	angular.module('todoapp', ['todoapp.routers','todoapp.controllers', 'ngCookies', 'oc.lazyLoad', 'toaster', 'ngAnimate'])
 		.config(['$ocLazyLoadProvider', function($ocLazyLoadProvider) {
 			$ocLazyLoadProvider.config({
 				debug  : true, // disable for production (it shows token in console log)
