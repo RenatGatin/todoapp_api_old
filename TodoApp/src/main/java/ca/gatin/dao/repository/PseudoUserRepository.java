5dfdc82d540b8f46f202dc6c15e3876a5eefd7f0
Renat Gatin <renat.gatin@gmail.com>
2018-08-31 00:51:55 -0700

    Fix pseudo_user DB method

--- TodoApp/src/main/java/ca/gatin/dao/repository/PseudoUserRepository.java
+++ TodoApp/src/main/java/ca/gatin/dao/repository/PseudoUserRepository.java
@@ -20,28 +20,28 @@ import ca.gatin.model.security.PseudoUser;
 @Repository
 public interface PseudoUserRepository extends JpaRepository<PseudoUser, Long> {
 
-	@Query(value = "SELECT * FROM user WHERE LOWER(username) = LOWER(:username) LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM pseudo_user WHERE LOWER(username) = LOWER(:username) LIMIT 1", nativeQuery = true)
 	PseudoUser findByUsernameCaseInsensitive(@Param("username") String username);
 	
-	@Query(value = "SELECT * FROM user WHERE LOWER(activationkey) = LOWER(:activationkey) LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM pseudo_user WHERE LOWER(activationkey) = LOWER(:activationkey) LIMIT 1", nativeQuery = true)
 	PseudoUser findByActivationKeyCaseInsensitive(@Param("activationkey") String username);
 
-	@Query(value = "SELECT * FROM user WHERE LOWER(email) = LOWER(:email) LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM pseudo_user WHERE LOWER(email) = LOWER(:email) LIMIT 1", nativeQuery = true)
 	PseudoUser findByEmailCaseInsensitive(@Param("email") String email);
 
-	@Query(value = "SELECT COUNT(*) FROM user WHERE LOWER(activationkey) = LOWER(:activationkey)", nativeQuery = true)
+	@Query(value = "SELECT COUNT(*) FROM pseudo_user WHERE LOWER(activationkey) = LOWER(:activationkey)", nativeQuery = true)
 	int countByActivationKey(@Param("activationkey") String activationkey);
 	
-	@Query(value = "SELECT COUNT(*) FROM user WHERE LOWER(username) = LOWER(:username)", nativeQuery = true)
+	@Query(value = "SELECT COUNT(*) FROM pseudo_user WHERE LOWER(username) = LOWER(:username)", nativeQuery = true)
 	int countByUsername(@Param("username") String username);
 
-	@Query(value = "SELECT COUNT(*) FROM user WHERE LOWER(email) = LOWER(:email)", nativeQuery = true)
+	@Query(value = "SELECT COUNT(*) FROM pseudo_user WHERE LOWER(email) = LOWER(:email)", nativeQuery = true)
 	int countByEmail(@Param("email") String email);
 
 	List<PseudoUser> findByActivated(boolean activated);
 
 	@Modifying
 	@Transactional
-	@Query(value = "UPDATE user SET activated = :activate WHERE id = :id", nativeQuery = true)
+	@Query(value = "UPDATE pseudo_user SET activated = :activate WHERE id = :id", nativeQuery = true)
 	int activate(@Param("activate") boolean activate, @Param("id") Long id);
 }
