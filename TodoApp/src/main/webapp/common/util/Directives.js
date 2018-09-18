210bbeb683eaf9a53d25aff634fe2664bfb1003d
Renat Gatin <renat.gatin@gmail.com>
2018-09-17 23:16:51 -0700

    passwords match validation

--- TodoApp/src/main/webapp/common/util/Directives.js
+++ TodoApp/src/main/webapp/common/util/Directives.js
@@ -1,5 +1,24 @@
 (function(angular) {
 	angular.module("todoapp")
+	
+	/**
+	 * New passwords match validation
+	 */
+	.directive('pwdsMatchCheck', [function () {
+	    return {
+	      require: 'ngModel',
+	      link: function (scope, elem, attrs, ctrl) {
+	        var firstPassword = '#' + attrs.pwdsMatchCheck;
+	        elem.add(firstPassword).on('keyup', function () {
+	          scope.$apply(function () {
+	            var v = elem.val()===$(firstPassword).val();
+	            ctrl.$setValidity('pwmatch', v);
+	            elem[0].setCustomValidity((v) ? '' : 'Passwords do not match.');
+	          });
+	        });
+	      }
+	    }
+	}])
 
 	.directive('validFile', function() {
 		return {
